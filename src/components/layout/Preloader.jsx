6987ebc3969942a4c174133352a8ae68d609f875
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-23 14:44:29 +0530

    connected redux to Logs component

--- src/components/layout/Preloader.jsx
+++ src/components/layout/Preloader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Preloader = () => {
   return (
     <div className='progress blue lighten-4'>
-      <div className='indeterminate' />
+      <div className='indeterminate blue darken-2' />
     </div>
   );
 };
